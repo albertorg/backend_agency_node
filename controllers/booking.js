971cbcfbed7231137a27b6bb179a96e2c935e113
Alberto Rodriguez <ticoc911@gmail.com>
2023-03-29 19:32:59 +0200

    availability route done

--- controllers/booking.js
+++ controllers/booking.js
@@ -6,19 +6,31 @@ const Hotels = require('../models/api_models/Hotels');
 
 const getAvailability = async (req = request, res = response) => {
 
-    const { codes = null } = req.query
+    try {
+        const booking = req.body
 
-    const hotels = await Promise.all(codes.map(async (code) => {
+        const config = {
+            method: 'post',
+            url: `${process.env.DOMAIN}/hotel-api/1.0/hotels`,
+            headers: {
+                'Api-key': process.env.API_KEY,
+                'X-Signature': xSignature(),
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            data: booking
+        };
 
-        const hotel = await Hotels.find({ 'code': code })
-        return hotel[0]
-    }))
+        const { data } = await axios(config)
 
-    res.json({
-        ok: true,
-        msg: 'getHotelsDetails',
-        hotels
-    })
+        res.send(data)
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Internal server error'
+        })
+    }
 }
 
 const getConfirmation = async (req = response, res = response) => {
@@ -57,7 +69,7 @@ const checkRate = async (req = response, res = response) => {
 
         const config = {
             method: 'post',
-            url: `${process.env.DOMAIN_SECURE}/hotel-api/1.0/checkrates`,
+            url: `${process.env.DOMAIN}/hotel-api/1.0/checkrates`,
             headers: {
                 'Api-key': process.env.API_KEY,
                 'X-Signature': xSignature(),
